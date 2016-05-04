b897047d8d9af648ec618b0c6f5eae73034f5333
miriamzfer <miriamzaragozafernandez@gmail.com>
2016-05-04 15:43:02 +0200

    actualizacion cambios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,36 @@
-//GET/quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+
+
+//GET/quizes/:id
+exports.show = function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show', { quiz: quiz});
+	})
+	
 };
 
-//GET/quizes/answer
+//GET/quizes/:id/answer
 exports.answer = function(req, res){
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+	if(req.query.respuesta === quiz.respuesta){
+		res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto'});
 	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		res.render('quizes/answer', { quiz: quiz, respuesta: 'Incorrecto'});
 	}
+})
+};
+
+//GET/quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', { quizes:quizes});
+	})
+};
+
+
+
+
+exports.author = function(req, res){
+	res.render('author', {});
 };
---
+++ models/models.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+//Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+
+
+//Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+
+//Importar la definicion de la tabla Quiz en quiz.js
+
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+exports.Quiz = Quiz;  //exportar definicion de tabla Quiz
+
+//sequelize.sync() cre e inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+	//succes(..) ejecuta el manejador una vez creada la tabla
+    Quiz.count().then(function (count){
+    	if(count === 0) {	//la tabla solo se inicializa si esta vacia
+    		Quiz.create({ pregunta: 'Capital de Italia',
+    					  respuesta: 'Roma'});
+    		Quiz.create({ pregunta: 'Capital de Portugal',
+    					  respuesta: 'Lisboa'})
+			.then(function(){console.log('Base de datos inicializada')});
+    	};
+    });
+
+});
+
